594e77f0152239057b79685f871c485cb95edf8b
matthieugras63 <matthieu.gras.63@gmail.com>
2019-09-09 11:30:58 +0200

    functions concatenated

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 $(function(){
   if($('button').hasClass("inactive")){
-    $('button').bind('mouseenter',function(){
-      $('#burger_menu div:first-child').css('transform', 'translateY(7px)').css('transition-duration','100ms');
-      $('#burger_menu div:last-child').css('transform', 'translateY(-7px)').css('transition-duration','100ms');
-    });
+    $('button').bind('mouseenter',translateToMiddle);
   }
   if($('button').hasClass("inactive")){
-    $('button').bind('mouseleave',function(){
-      $('#burger_menu div:first-child').css('transform', 'translateY(0px)').css('transition-duration','100ms');
-      $('#burger_menu div:last-child').css('transform', 'translateY(0px)').css('transition-duration','100ms');
-    });
+    $('button').bind('mouseleave',translateToOrigin);
   }
 });
-
-
 
 $(function(){
   $('#burger_menu button').click(function(){
     if ($('button').hasClass("active")){
-      $('#burger_menu div:first-child').width('70%').css('transform', 'rotate(0deg) translateY(7px)').css('transition-duration','100ms').css('transform-origin','center').css('background-color','black');
-      $('#burger_menu div:last-child').css('transform', 'rotate(0deg) translateY(-7px)').css('transition-duration','100ms').css('transform-origin','center').css('background-color','black');
-      $('button').css('transform','translateX(0px)').css('transition-duration', '100ms');
-      $('#burger_menu button').removeClass("active").addClass("inactive");
-      $('button').bind('mouseenter',function(){
-        $('#burger_menu div:first-child').css('transform', 'translateY(7px)').css('transition-duration','100ms');
-        $('#burger_menu div:last-child').css('transform', 'translateY(-7px)').css('transition-duration','100ms');
-      });
-      $('button').bind('mouseleave',function(){
-        $('#burger_menu div:first-child').css('transform', 'translateY(0px)').css('transition-duration','100ms');
-        $('#burger_menu div:last-child').css('transform', 'translateY(0px)').css('transition-duration','100ms');
-      });
+      crossToBars();
+      $('button').bind('mouseenter',translateToMiddle);
+      $('button').bind('mouseleave',translateToOrigin);
     }else{
-      $('#burger_menu div:first-child').width('100%').css('transform', 'rotate(45deg) translateY(10px)').css('transition-duration','100ms').css('transform-origin','center').css('background-color','red');
-      $('#burger_menu div:last-child').css('transform', 'rotate(-45deg) translateY(-10px)').css('transition-duration','100ms').css('transform-origin','center').css('background-color','red');
-      $('#burger_menu button').removeClass("inactive").addClass("active");
-      $('button').css('transform','translateX(5px)').css('transition-duration', '100ms');
-      $('button').unbind('mouseleave').unbind('mouseenter');
+      createCross();
     }
   });
 });
 
+
+
+function translateToMiddle(){
+  $('#burger_menu div:first-child').css('transform', 'translateY(7px)').css('transition-duration','200ms');
+  $('#burger_menu div:last-child').css('transform', 'translateY(-7px)').css('transition-duration','200ms');
+}
+
+function translateToOrigin(){
+ $('#burger_menu div:first-child').css('transform', 'translateY(0px)').css('transition-duration','200ms');
+ $('#burger_menu div:last-child').css('transform', 'translateY(0px)').css('transition-duration','200ms');
+}
+
+function createCross(){
+  $('#burger_menu div:first-child').width('100%').css('transform', 'rotate(45deg) translateY(10px)').css('transition-duration','200ms').css('transform-origin','center').css('background-color','red');
+  $('#burger_menu div:last-child').css('transform', 'rotate(-45deg) translateY(-10px)').css('transition-duration','200ms').css('transform-origin','center').css('background-color','red');
+  $('#burger_menu button').removeClass("inactive").addClass("active");
+  $('button').css('transform','translateX(5px)').css('transition-duration', '200ms');
+  $('button').unbind('mouseleave').unbind('mouseenter');
+}
+
+function crossToBars(){
+  $('#burger_menu div:first-child').width('70%').css('transform', 'rotate(0deg) translateY(7px)').css('transition-duration','200ms').css('transform-origin','center').css('background-color','black');
+  $('#burger_menu div:last-child').css('transform', 'rotate(0deg) translateY(-7px)').css('transition-duration','200ms').css('transform-origin','center').css('background-color','black');
+  $('button').css('transform','translateX(0px)').css('transition-duration', '200ms');
+  $('#burger_menu button').removeClass("active").addClass("inactive");
+}
